c126ec17b79d5c86674cb8e1f67e67d87882ec0d
Rayane Pereira <89112313+RayanePereira@users.noreply.github.com>
2022-08-27 22:49:16 -0300

    Create adicionar-campos.js

---
+++ adicionar-campos.js
@@ -0,0 +1,16 @@
+const cliente = {
+  nome: "Andre",
+  idade: 36,
+  cpf: "01234567892142",
+  email: "andre@email.com"
+}
+
+console.log(cliente);
+
+cliente.fone = "000000000"
+
+console.log(cliente);
+
+cliente.fone: "1111111111"
+
+console.log(cliente)
