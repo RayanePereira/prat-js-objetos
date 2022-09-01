c181a9147ec272544af218a52d28a6f4623684bc
Rayane Pereira <89112313+RayanePereira@users.noreply.github.com>
2022-08-31 22:11:04 -0300

    Create print-infos.js

---
+++ print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+  nome:"João",
+  idade:21,
+  cpf:"000000000",
+  email:"joao@email.com"
+}
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`)
+
+console.log(cliente.cpf.substring(0,3))
