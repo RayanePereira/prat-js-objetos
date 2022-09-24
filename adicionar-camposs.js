8c95a127ac5ea006d86765cff31aeebfb1c0700b
Rayane Pereira <89112313+RayanePereira@users.noreply.github.com>
2022-09-24 19:22:29 -0300

    Create adicionar-camposs.js

---
+++ adicionar-camposs.js
@@ -0,0 +1,15 @@
+const cliente = {
+  nome: "João";
+  idade: 21,
+  cpf: "00000000000",
+  email: "joao@gmail.com"
+}
+console.log(cliente);
+
+cliente.fone = "000000000"
+
+console.log(cliente);
+
+cliente.fone = "111111111"
+
+console.log(cliente)
