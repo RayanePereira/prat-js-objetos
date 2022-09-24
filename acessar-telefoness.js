d49fd8170b0e05624ab378f2aa92fb548bc6cc9b
Rayane Pereira <89112313+RayanePereira@users.noreply.github.com>
2022-09-23 22:22:25 -0300

    Create acessar-telefoness.js

---
+++ acessar-telefoness.js
@@ -0,0 +1,9 @@
+const cliente = {
+  nome: "João",
+  idade: 21,
+  cpf: "0000000000",
+  email: "joao@gmail.com",
+  fones: ["00000000", "000000000"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))
