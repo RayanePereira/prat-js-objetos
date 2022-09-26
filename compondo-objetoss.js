fbb94ac8bba7a05cd9723724d4094a2d4201191f
Rayane Pereira <89112313+RayanePereira@users.noreply.github.com>
2022-09-25 21:52:59 -0300

    Create compondo-objetoss.js

---
+++ compondo-objetoss.js
@@ -0,0 +1,19 @@
+const clinete = {
+  nome: "João",
+  idade: 21,
+  cpf: "00000000000",
+  email: "joao@email.com",
+  fones: ["000000000", "000000000000"]
+}
+
+clinete.dependentes ={
+  nome: "Aurora",
+  parentesco: "filha",
+  dataNasc: "21/07"
+}
+
+console.log(cliente)
+
+clinete.dependentes.nome = "Aurora"
+
+console.log(cliente)
